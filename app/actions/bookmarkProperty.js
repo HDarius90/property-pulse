338fe0b726ecaaf8a33ea773f1df21bed46e784e
Hadjati Darius <72970048+HDarius90@users.noreply.github.com>
2025-07-24 21:49:59 +0200

    add bookmark property server action

---
+++ app/actions/bookmarkProperty.js
@@ -0,0 +1,45 @@
+'use server';
+import connectDB from '@/config/database';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache';
+
+async function bookmarkProperty(propertyId) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error('User ID is required');
+  }
+
+  const { userId } = sessionUser;
+
+  const user = await User.findById(userId);
+
+  const isBookmarked = user.bookmarks.includes(propertyId);
+
+  let message;
+
+  if (isBookmarked) {
+    // If already bookmarked, then remove
+    user.bookmarks.pull(propertyId);
+    message = 'Property removed from bookmarks';
+    isBookmarked = false;
+  } else {
+    // If not bookmarked, then add
+    user.bookmarks.push(propertyId);
+    message = 'Property added to bookmarks';
+    isBookmarked = true;
+  }
+
+  await user.save();
+  revalidatePath('/properties/saved', 'page');
+
+  return {
+    message,
+    isBookmarked,
+  };
+}
+
+export default bookmarkProperty;
